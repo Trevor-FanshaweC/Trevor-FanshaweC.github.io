19807098674e88c29ace72b433456817de84c16f
Trevor - Fanshawe <tvanrys@fanshaweonline.ca>
2022-11-21 13:08:51 -0500

    testing

---
+++ js/main.js
@@ -0,0 +1,22 @@
+(() => {
+    
+    console.log('JS seems to be working');
+
+    let stuff = [
+        {
+            name: 'item1',
+            desc: 'item 1 stuff'
+        },
+
+        {
+            name: 'item2',
+            desc: 'item 2 stuff'
+        },
+
+        {
+            name: 'item3',
+            desc: 'item 3 stuff'
+        },
+
+    ]
+})();
