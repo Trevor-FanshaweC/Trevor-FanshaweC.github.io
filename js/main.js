b67d6c6f9983980c9cae292524849b7058e3ac0f
Trevor - Fanshawe <tvanrys@fanshaweonline.ca>
2022-11-21 13:30:54 -0500

    dynamic stuff

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 (() => {
-    
+
     console.log('JS seems to be working');
+
+    let container = document.querySelector('.dynamic');
 
     let stuff = [
         {
@@ -19,4 +21,33 @@
         },
 
     ]
+
+    function addStuff() {
+        let myNewStuff = new DocumentFragment();
+
+        stuff.forEach(thing => {
+            let theHeading = document.createElement('h2');
+            theHeading.textContent = thing.name;
+
+            let theDesc = document.createElement('p');
+            theDesc.textContent = thing.desc;
+
+            myNewStuff.appendChild(theHeading);
+            myNewStuff.appendChild(theDesc);
+        })
+
+        // for (thing of stuff) {
+        //     let newContent = `
+        //     <div>
+        //         <p>${thing.name}</p>
+        //         <p>${thing.desc}</p>
+        //     </div>`;
+
+        //     myNewStuff.innerHTML += newContent;
+        // }
+
+        container.appendChild(myNewStuff);
+    }
+
+    addStuff();
 })();
